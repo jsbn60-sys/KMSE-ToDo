2669995cb28a9275889a71fb2c7a9f9cf14bbec0
Marvin <seckeichhorn@gmail.com>
2020-05-10 15:45:20 +0200

    minor change TaskController

--- src/main/kotlin/javalinvue/TaskController.kt
+++ src/main/kotlin/javalinvue/TaskController.kt
@@ -3,11 +3,10 @@ package javalinvue
 import io.javalin.http.Context
 import io.javalin.http.NotFoundResponse
 
-data class Task(val id: Int, val title: String, val user: User, val category: Category?)
-data class TaskDetails(val category: Category?, val checked: Boolean, val priority: Int)
+data class Task(val id: Int, val title: String, val user: User, val category: Category?, val checked: Boolean, val priority: Int)
 
 val tasks = setOf<Task>(
-    Task(id = 1, title = "John", user = USEROBJECT, taskDetails = TaskDetails(CATEGORYOBJECT, false, 10 ))
+    Task(id = 1, title = "Task", user = USEROBJECT, category = CATEGORYOBJECT, checked = false, priority = 10)
     )
 
 object taskController {
